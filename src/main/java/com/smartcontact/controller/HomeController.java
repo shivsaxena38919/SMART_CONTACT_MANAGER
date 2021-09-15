c910ef36b3216708b185d6bf2f65c996bb458aeb
Shivam Saxena <shivam.saxena@ericsson.com>
2021-09-15 21:11:48 +0530

    new changes v1

--- src/main/java/com/smartcontact/controller/HomeController.java
+++ src/main/java/com/smartcontact/controller/HomeController.java
@@ -7,10 +7,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 	
-	@RequestMapping("/home")
+	@RequestMapping("/")
 	public String home() {
 		
 		return "home";
 	}
+	
+	@RequestMapping("/signup")
+	public String signup() {
+		
+		return "signup";
+	}
+
 
 }
